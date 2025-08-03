13302a48a45d65dc42b72f246876b50ba5c9ac3d
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2025-08-03 23:56:02 +0100

    Add firebase and firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:blood_glucose_monitor/firebase_options.dart';
 import 'package:blood_glucose_monitor/pages/onboarding.dart';
 import 'package:blood_glucose_monitor/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
