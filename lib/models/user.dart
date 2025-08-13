c08794bcaca7d9846895b750d69637cc611b8e82
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2025-08-13 18:23:12 +0100

    Update user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,10 +1,10 @@
-class User {
+class UserProfile {
   final String id;
   final String email;
   final String username;
   final String profilePhoto;
 
-  const User({
+  const UserProfile({
     this.id = '000',
     this.email = '',
     this.username = '_glucoseboy',
