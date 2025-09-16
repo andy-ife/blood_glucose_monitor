253faee9dbb520555df4fd83e48b91188e2c6e31
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2025-09-16 09:34:43 +0100

    Add refresh indicator

--- lib/pages/home/dashboard.dart
+++ lib/pages/home/dashboard.dart
@@ -72,61 +72,67 @@ class DashboardPage extends StatelessWidget {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsetsGeometry.symmetric(horizontal: 16.0),
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 80.0),
-          child: Column(
-            spacing: 20.0,
-            children: [
-              state.loading
-                  ? ShimmerLoading(
-                      height: constraints.height * 0.16,
-                      width: double.infinity,
-                      borderRadius: BorderRadius.circular(24.0),
-                    )
-                  : state.currentTest == null
-                  ? SizedBox.shrink()
-                  : CurrentTestCard(reading: state.currentTest!),
-              state.loading
-                  ? ShimmerLoading(
-                      height: constraints.height * 0.36,
-                      width: double.infinity,
-                    )
-                  : GlucoseChart(readings: state.todayTests),
-              Column(
-                spacing: 16.0,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text('Your Tests', style: theme.textTheme.titleLarge),
-                      InkWell(
-                        onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (_) => TestsPage()),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Text(
-                            'See all',
-                            style: theme.textTheme.titleMedium!.copyWith(
-                              fontWeight: FontWeight.w600,
-                              color: theme.colorScheme.primary,
+      body: RefreshIndicator(
+        onRefresh: () async => controller.init(),
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          padding: EdgeInsetsGeometry.symmetric(horizontal: 16.0),
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 80.0),
+            child: Column(
+              spacing: 20.0,
+              children: [
+                state.loading
+                    ? ShimmerLoading(
+                        height: constraints.height * 0.16,
+                        width: double.infinity,
+                        borderRadius: BorderRadius.circular(24.0),
+                      )
+                    : state.currentTest == null
+                    ? SizedBox.shrink()
+                    : CurrentTestCard(reading: state.currentTest!),
+                state.loading
+                    ? ShimmerLoading(
+                        height: constraints.height * 0.36,
+                        width: double.infinity,
+                      )
+                    : GlucoseChart(readings: state.todayTests),
+                Column(
+                  spacing: 16.0,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text('Your Tests', style: theme.textTheme.titleLarge),
+                        InkWell(
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (_) => TestsPage()),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0,
+                            ),
+                            child: Text(
+                              'See all',
+                              style: theme.textTheme.titleMedium!.copyWith(
+                                fontWeight: FontWeight.w600,
+                                color: theme.colorScheme.primary,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  ...List.generate(
-                    state.recentTests.length,
-                    (i) => RecentTestCard(reading: state.recentTests[i]),
-                  ),
-                ],
-              ),
-            ],
+                      ],
+                    ),
+                    ...List.generate(
+                      state.recentTests.length,
+                      (i) => RecentTestCard(reading: state.recentTests[i]),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
